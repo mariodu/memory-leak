1a3a50b938cf07683718aaf2f39c8ea4921fa929
mariodu <dujiakun@gmail.com>
2016-04-14 17:37:28 +0800

    fix: some spell

--- leak-memory.js
+++ leak-memory.js
@@ -5,7 +5,7 @@ const path = require('path');
 const heapdump = require('heapdump');
 
 // 模拟不会释放的内容，一直持有对象的引用
-let noRelease = [];
+let neverRelease = [];
 // 会释放的内容
 let willRelease = [];
 let usage = [];
@@ -20,7 +20,7 @@ class Foo {
 function generatorData() {
 	let obj = new Foo();
 
-	noRelease.push(obj);
+	neverRelease.push(obj);
 	willRelease.push(obj);
 
 	// 简单操作对象
@@ -36,31 +36,31 @@ function doSomething(obj) {
 function cleanUpData(obj) {
 	let index = willRelease.indexOf(obj);
 	// 清除掉 willRelease 里对对象的引用
-	// “忘了”清理 noRelease 中的引用
+	// “忘了”清理 neverRelease 中的引用
 	willRelease.splice(index, 1);
-    // noRelease.splice(index, 1);
+  // neverRelease.splice(index, 1);
 	// 一个对象，并且没被释放
-	// console.log(`[noRelease] obj ${index} 是否使用过：${noRelease[index].used}`);
+	// console.log(`[neverRelease] obj ${index} 是否使用过：${neverRelease[index].used}`);
 }
 
 function runtimeInfo() {
-	// 收到触发 GC
+	// 手动触发 GC
 	global.gc();
 
 	// 输出内存占用
 	let heapUsed = process.memoryUsage().heapUsed;
 	usage.push(heapUsed);
-  	console.log(`Program is using ${heapUsed} bytes of Heap.`);
+	console.log(`堆占用 ${heapUsed} bytes.`);
 
-  	// 触发生成 heapdump 文件
-  	//heapdump.writeSnapshot(path.join(__dirname, `heapdump-${Date.now()}.heapsnapshot`));
-  	process.kill(process.pid, 'SIGUSR2');
+	// 触发生成 heapdump 文件
+	//heapdump.writeSnapshot(path.join(__dirname, `heapdump-${Date.now()}.heapsnapshot`));
+	process.kill(process.pid, 'SIGUSR2');
 }
 
 // 5ms 生成一次数据
 setInterval(generatorData, 5);
 // 2s 清理一次并打出占用
-setInterval(runtimeInfo, 2000); 
+setInterval(runtimeInfo, 2000);
 
 process.on('SIGINT', function(){
   let data = JSON.stringify(usage);
